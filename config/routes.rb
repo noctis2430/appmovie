2b523747650455dc8b74aec8a52bdb4c9571bb54
Luis <luis_fcb@live.com>
2017-05-31 17:57:07 +0200

    añadir rutas

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
 
   get 'peliculas/index'
 
+  root :to => 'statics#index'
+
+  get '/about' => 'static#about', :as => :about
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
