d45aedc8b4c2a99e8bc0c9fc5c358c4311074c05
Luis <luis_fcb@live.com>
2017-05-31 17:41:58 +0200

    first commit

---
+++ app/controllers/peliculas_controller.rb
@@ -0,0 +1,50 @@
+class PeliculasController < ApplicationController
+  	def index
+		@peliculas = Pelicula.all
+	end
+
+	def show
+   		@pelicula = Pelicula.find(params[:id])
+   		if @pelicula.name == "Cenicienta"
+   			@cenicienta = true
+   		else
+   			@cenicienta = false
+   		end
+   	end
+
+	def new
+		@pelicula = Pelicula.new
+	end
+
+	def create
+		@pelicula = Pelicula.new(peli_params)
+		if @pelicula.save
+			redirect_to peliculas_path, :notice => "Se ha guardado la peli!"
+		else
+			render "new"
+		end
+	end
+
+	def edit
+		@pelicula = Pelicula.find(params[:id])
+	end
+
+	def update
+		@pelicula = Pelicula.find(params[:id])
+		if @pelicula.update_attributes(peli_params)
+			redirect_to peliculas_path, :notice => "exito!"
+		else
+			render "edit"
+		end
+	end
+
+	def destroy
+		@pelicula = Pelicula.find(params[:id])
+		@pelicula.destroy
+		redirect_to peliculas_path, :notice => "Borrado"
+	end
+	private
+      def peli_params
+         params.require(:pelicula).permit(:name, :stars, :year, :director)
+      end
+end
