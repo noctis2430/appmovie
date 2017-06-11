ffa1905abc25f5ad90d64987e3c410a54c62ffea
luis <luis_fcb@live.com>
2017-06-11 19:48:00 +0200

    funcionament i vistes settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,6 +2,53 @@ class SettingsController < ApplicationController
 
 	layout "admin"
 	
-  def index
-  end
+	def index
+ 		@setting = current_user.setting
+	end
+ 
+	def show
+	  	@setting = Setting.find(params[:id])
+	end
+
+	def new
+    	@setting = current_user.build_setting
+	end
+
+
+
+	def create
+		@setting = current_user.build_setting(setting_params)
+
+		if @setting.save
+			redirect_to settings_path
+		else
+			render :new
+		end
+	end
+
+	 
+	def edit
+	  	@setting = Setting.find(params[:id])
+	end
+
+	def update
+ 		@setting = Setting.find(params[:id])
+ 
+  		if @setting.update_attributes(setting_params)
+   			redirect_to settings_path
+  		else
+			render :index
+  		end
+	end
+
+	def destroy
+		@setting = Setting.find(params[:id])
+		@setting.destroy
+		redirect_to settings_path, :notice => "Borrado"
+	end
+
+	private
+	def setting_params
+  		params.require(:setting).permit(:country, :language)
+	end 
 end
